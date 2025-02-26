d1ccf26223687ca03f47b8b6cffe2303148aaa64
SadLun <sadluntic@gmail.com>
2025-02-26 20:06:08 +0300

    first commit

---
+++ diploma-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux'
+import store from './store/store'
+
+
+function App() {
+
+  return (
+    <Provider store={store}>
+      <div>aha</div>
+    </Provider>
+  )
+}
+
+export default App
