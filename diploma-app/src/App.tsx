60706e9b7838bbf45c404b655e407d6b547bd2df
SadLun <sadluntic@gmail.com>
2025-04-22 20:08:33 +0300

    navbar added

--- diploma-app/src/App.tsx
+++ diploma-app/src/App.tsx
@@ -1,8 +1,8 @@
 import { Provider } from 'react-redux'
 import store from './store/store'
 import { Container, Typography } from '@mui/material'
-import Table from './components/tableCell'
 import useMiddleClickSearch from './components/middleMouseClick'
+import NavTabs from './components/navbar'
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <Provider store={store}>
       <Container maxWidth="md">
         <Typography component="h1" variant="h2" align="center">Device DataBase</Typography>
-        <Table />
+        <NavTabs />
       </Container>
     </Provider>
   )
---
+++ diploma-app/src/components/navbar.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Statistics from './statistics'
+import Table from './tableCell'
+
+function samePageLinkNavigation(
+  event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+) {
+  if (
+    event.defaultPrevented ||
+    event.button !== 0 || // ignore everything but left-click
+    event.metaKey ||
+    event.ctrlKey ||
+    event.altKey ||
+    event.shiftKey
+  ) {
+    return false;
+  }
+  return true;
+}
+
+interface LinkTabProps {
+  label?: string;
+  href?: string;
+  selected?: boolean;
+}
+
+function LinkTab(props: LinkTabProps) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        if (samePageLinkNavigation(event)) {
+          event.preventDefault();
+        }
+      }}
+      aria-current={props.selected && 'page'}
+      {...props}
+    />
+  );
+}
+
+export default function NavTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    // event.type can be equal to focus with selectionFollowsFocus.
+    if (
+      event.type !== 'click' ||
+      (event.type === 'click' &&
+        samePageLinkNavigation(
+          event as React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+        ))
+    ) {
+      setValue(newValue);
+    }
+  };
+
+  let component;
+
+  if (value === 0) {
+    component = <Table />;
+  } else if (value === 1) {
+    component = <Statistics />;
+  }
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        role="navigation"
+      >
+        <LinkTab label="Главная" href="/tableCell" />
+        <LinkTab label="Статистика" href="/statistics" />
+      </Tabs>
+      {component}
+    </Box>
+  );
+}
