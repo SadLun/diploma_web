80dc3c1a676b3ff322a2a40a3c67321dbc85cadf
Максим <mdkolodin@miem.hse.ru>
2025-04-27 22:17:59 +0300

    added graph

--- diploma-app/src/components/statistics.tsx
+++ diploma-app/src/components/statistics.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 const Statistics: React.FC = () => {
-    return <div>Statistics</div>
+    return (
+        <BarChart
+          xAxis={[{ scaleType: 'band', data: ['group A'] }]}
+          series={[{ data: [4] }, { data: [1] }, { data: [2] }]}
+          height={500}
+        />
+      );
 }
 
 export default Statistics
